72938420a78c20fd18e7fb9a6a0421eefcc1e287
Brianca Knight <briancaknight@gmail.com>
2024-02-15 11:42:48 -0800

    add parameters to get requests

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -17,9 +17,26 @@ namespace CretaceousApi.Controllers
 
     // GET api/animals
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    public async Task<List<Animal>> Get(string species, string name, int minimumAge)
     {
-      return await _db.Animals.ToListAsync();
+      IQueryable<Animal> query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (minimumAge > 0)
+      {
+        query = query.Where(entry => entry.Age >= minimumAge);
+      }
+
+      return await query.ToListAsync();
     }
 
     // GET: api/Animals/5
@@ -36,16 +53,16 @@ namespace CretaceousApi.Controllers
       return animal;
     }
 
-       // POST api/animals
+    // POST api/animals
     [HttpPost]
-    public async Task<ActionResult<Animal>> Post(Animal animal)
+    public async Task<ActionResult<Animal>> Post([FromBody] Animal animal)
     {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
     }
 
-    // PUT: api/Animals/5
+        // PUT: api/Animals/5
     [HttpPut("{id}")]
     public async Task<IActionResult> Put(int id, Animal animal)
     {
@@ -80,7 +97,7 @@ namespace CretaceousApi.Controllers
       return _db.Animals.Any(e => e.AnimalId == id);
     }
 
-     // DELETE: api/Animals/5
+    // DELETE: api/Animals/5
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteAnimal(int id)
     {
